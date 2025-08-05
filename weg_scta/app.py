b4ee5e89a6d344ab71fe211b629efa1d58de3729
mateusscavalheiro-afk <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2025-08-05 11:59:46 -0300

    Update app.py

--- weg_scta/app.py
+++ weg_scta/app.py
@@ -39,5 +39,8 @@ def rota_listar():
     df = listar_produtos()
     return jsonify(df.to_dict(orient="records"))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
